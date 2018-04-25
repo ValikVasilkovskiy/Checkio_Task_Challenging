d4128898189f5a9e261fff48c76a6d839e9972d1
ValikVasilkovskiy <valik.vasilkovskiy@gmail.com>
2018-04-25 17:00:07 +0300

    Open Labyrinth 1-4 check - ok

--- Open_Labyrinth.py
+++ Open_Labyrinth.py
@@ -1,24 +1,27 @@
-
-def open_labyrinth(data):
+def open_labyrinth(maze_map):
     direct = []
     # start point
     row, col = 1, 1
     def dfs(row, col):
-        data[row][col] = 2
-        direction_dict = {'S': [row+1, col], 'N': [row-1, col], 'E': [row, col+1], 'W': [row, col-1]}
+        check = 0
+        maze_map[row][col] = 2
+        direction_dict = {'S': [row+1, col], 'E': [row, col+1], 'N': [row-1, col], 'W': [row, col-1]}
         # k - dict key
         for k, v in direction_dict.items():
             i, j = v[0], v[1]
             if i == 10 and j == 10:
                 direct.append(k)
                 direct.append('end')
+                check += 1
             else:
-                if data[i][j] != 2 and data[i][j] == 0:
+                if maze_map[i][j] != 2 and maze_map[i][j] == 0:
                     direct.append(k)
+                    check += 1
                     dfs(i, j)
-        
+        if check == 0:
+            direct.pop(-1)
     dfs(row, col)
-    return ''.join(direct[0:direct.index('end')])
+    return ''.join(direct[0 : direct.index('end')])
 
 print(open_labyrinth([
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -33,6 +36,19 @@ print(open_labyrinth([
         [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
         [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]))
+print(open_labyrinth([
+        [1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,1,0,0,0,1,0,0,1],
+        [1,0,1,0,0,0,1,0,0,0,1,1],
+        [1,0,0,0,1,0,0,0,1,0,0,1],
+        [1,0,1,0,0,0,1,0,0,0,1,1],
+        [1,0,0,0,1,0,0,0,1,0,0,1],
+        [1,0,1,0,0,0,1,0,0,0,1,1],
+        [1,0,0,0,1,0,0,0,1,0,0,1],
+        [1,0,1,0,0,0,1,0,0,0,1,1],
+        [1,0,0,0,1,0,0,0,1,0,0,1],
+        [1,0,1,0,0,0,1,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1]]))
 
 
 
