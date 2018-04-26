9cb7efd9c3407cf499e6b337343b10f7a2985274
ValikVasilkovskiy <valik.vasilkovskiy@gmail.com>
2018-04-26 17:47:10 +0300

    Safe Coasts initial commit

--- Safe_Coasts.py
+++ Safe_Coasts.py
@@ -1,7 +1,6 @@
 def finish_map(regional_map):
     regional_map = [(list(map(lambda x: x, i))) for i in regional_map]
     def dfs(row, col):
-        check = 0
         regional_map[row][col] = 'D'
         for i in range(row - 1, row + 2):
             for j in range(col - 1, col + 2):
@@ -12,12 +11,10 @@ def finish_map(regional_map):
                     try:
                         if regional_map[i][j] == 'X':
                             regional_map[row][col] = 'S'
-                            check += 1
                         elif regional_map[i][j] != 'D' and regional_map[i][j] == '.':
                             dfs(i, j)
                     except IndexError:
                         continue
-
     for i in range(len(regional_map)):
         for j in range(len(regional_map[i])):
             if regional_map[i][j] == 'D':
